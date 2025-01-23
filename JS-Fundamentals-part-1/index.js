ea0f4e50a18ebabc23efeca8918ff682aa198872
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-23 13:40:38 +0530

    operator,precedence,string

--- JS-Fundamentals-part-1/index.js
+++ JS-Fundamentals-part-1/index.js
@@ -31,47 +31,105 @@
 // console.log("type of javascriptisfun after:",typeof javaScriptisFun);
 
 
-//let (mutable)
-let age=30;
-console.log(age);
+// //let (mutable)
+// let age=30;
+// console.log(age);
 
-age = 36;
-console.log(age);
+// age = 36;
+// console.log(age);
 
 
-console.log("let-block ex :");
-{
-    let job = "programmer";
-    console.log(job);
+// console.log("let-block ex :");
+// {
+//     let job = "programmer";
+//     console.log(job);
     
 
-}
-console.log("outside block",job);
-job = "technician";
-console.log(job);
+// }
+// console.log("outside block",job);
+// job = "technician";
+// console.log(job);
 
 
-//const (immutable)
+// //const (immutable)
+// const birthYear = 2003;
+// console.log(birthYear);
+
+// //can't change. 
+// // birthYear = 2002;
+// // console.log(birthYear); 
+
+// // no empty consts
+// // const job; 
+
+
+// // var
+// console.log("car-block ex :");
+
+// {
+//     var job = "programmer";
+
+// }
+// console.log(job);
+// job = "technician";
+// console.log(job);
+
+
+
+// // math operators 
+
+// const currYear = 2024;
+// const ageKabir = currYear - 2003;
+// const ageUtsav = currYear - 2001;
+
+// console.log(ageKabir, ageUtsav);
+
+// console.log(ageKabir * 2 , ageUtsav / 2, 2**3);
+
+// const firstName = "kabir";
+// const lastName = "navadiya";
+
+// console.log("fullname : ",firstName+" "+lastName);
+
+// // assignment operators
+// let x = 10 + 5;
+// x+=10;
+// x*=4;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+
+// //comparison operators
+
+// console.log(ageKabir > ageUtsav); // >, < , >=, <= 
+
+
+// //challenge
+// let massMark = 78;
+// let massJohn = 92;
+// let heightMark = 1.69;
+// let heightJohn = 1.95;
+
+// BMIMark = massMark/(heightMark * heightMark);
+// BMIJohn = massJohn/(heightJohn * heightJohn);
+
+// console.log(BMIMark,BMIJohn);
+
+// const markHigherBMI = BMIMark > BMIJohn;
+// console.log(markHigherBMI);
+
+
+// string
+
+const firstName = "Kabir";
+const job = "Trainee Engineer";
 const birthYear = 2003;
-console.log(birthYear);
+const year = 2024;
+const kabir = "I'm " + firstName + ', a ' + (year-birthYear) + ' years old ' + job + '!';
+console.log(kabir);
 
-//can't change. 
-// birthYear = 2002;
-// console.log(birthYear); 
+//tempelate literals
 
-// no empty consts
-// const job; 
-
-// var
-
-
-console.log("car-block ex :");
-
-{
-    var job = "programmer";
-
-}
-console.log(job);
-job = "technician";
-console.log(job);
-
+const kabirnew = `I'm ${firstName}, a ${(year-birthYear)} years old ${job}!`;
+console.log(kabirnew);
