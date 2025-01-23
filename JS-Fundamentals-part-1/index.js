c98ddb817c95b9aefdb7ca5214960b3b59d2e2c0
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-23 14:28:31 +0530

    conversion,condition,equality op, switch

--- JS-Fundamentals-part-1/index.js
+++ JS-Fundamentals-part-1/index.js
@@ -120,16 +120,150 @@
 // console.log(markHigherBMI);
 
 
-// string
+// // string
 
-const firstName = "Kabir";
-const job = "Trainee Engineer";
-const birthYear = 2003;
-const year = 2024;
-const kabir = "I'm " + firstName + ', a ' + (year-birthYear) + ' years old ' + job + '!';
-console.log(kabir);
+// const firstName = "Kabir";
+// const job = "Trainee Engineer";
+// const birthYear = 2003;
+// const year = 2024;
+// const kabir = "I'm " + firstName + ', a ' + (year-birthYear) + ' years old ' + job + '!';
+// console.log(kabir);
 
-//tempelate literals
+// //tempelate literals
 
-const kabirnew = `I'm ${firstName}, a ${(year-birthYear)} years old ${job}!`;
-console.log(kabirnew);
+// const kabirnew = `I'm ${firstName}, a ${(year-birthYear)} years old ${job}!`;
+// console.log(kabirnew);
+
+
+// //conditional 
+
+// const age =19;
+
+// if(age>=18){
+//     console.log("Eligible for driver's license");
+    
+// }
+// else{
+//     console.log("Not eligible for driver's license");
+    
+// }
+
+// //challenge#2
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// if(BMIMark > BMIJohn){
+//     console.log("Mark's BMI is higher than John's!");
+// }
+// else{
+//     console.log("John's BMI is higher than Mark's!");
+// }
+// if(BMIMark > BMIJohn){
+//     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// }
+// else{
+//     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
+
+
+//conversion and coercion
+// const inputyear = '2003';
+// const curryear=2024;
+// const age = curryear - Number(inputyear);
+// console.log(age);
+
+// console.log( typeof String(21),String(21));
+
+// //type coercion
+// console.log("below all are strings");
+
+// console.log('I am ' + 21 + ' years old ');
+
+// console.log('23'-'10'-3);
+// console.log('2' * '3');
+// console.log('10' / '2');
+
+
+
+// 5 falsy values: 0, '', undefined, null,NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('kabir'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money=0;
+// if(money){
+//     console.log("Dont' spend it all !");
+// }else{
+//     console.log("you should get a job !");
+    
+// }
+
+// let height; // let height =156;
+// if(height){
+//     console.log("YAY ! Height is defined");
+// }else{
+//     console.log("Height is undefined !");
+    
+// }
+
+
+
+// Equality operators == & ===
+
+// let a = 1;
+// let b = '1';
+// if (a == b) {
+//     console.log("a==b true");
+    
+// } else {
+//     console.log("a==b false");
+    
+// }
+
+// if (a===b) {
+//     console.log("a===b true");
+    
+// } else {
+//     console.log("a===b false");
+    
+// }
+
+
+// //prompt
+
+// const favourite = prompt("enter your favourite number !");
+// console.log('favourite :',favourite);
+
+
+
+//switch statement
+
+const day =prompt("Enter a day");
+switch (day) {
+    case 'monday':
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+        break;
+    case 'tuesday':
+        console.log("Prepare theory videos");
+        break
+    case 'wednesday':
+    case 'thursday':
+        console.log("write code examples");
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log("write code examples");
+        break;
+    default:
+        console.log("Not a valid day!");
+        break;
+}
