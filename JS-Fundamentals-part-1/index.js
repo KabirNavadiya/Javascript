3395df58dd1030ddf4c1d7730eee3122efeb5bec
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-23 14:45:22 +0530

    ternary operator and challenge

--- JS-Fundamentals-part-1/index.js
+++ JS-Fundamentals-part-1/index.js
@@ -244,26 +244,46 @@
 
 
 
-//switch statement
+// //switch statement
 
-const day =prompt("Enter a day");
-switch (day) {
-    case 'monday':
-        console.log("Plan course structure");
-        console.log("Go to coding meetup");
-        break;
-    case 'tuesday':
-        console.log("Prepare theory videos");
-        break
-    case 'wednesday':
-    case 'thursday':
-        console.log("write code examples");
-        break;
-    case 'saturday':
-    case 'sunday':
-        console.log("write code examples");
-        break;
-    default:
-        console.log("Not a valid day!");
-        break;
-}
+// const day =prompt("Enter a day");
+// switch (day) {
+//     case 'monday':
+//         console.log("Plan course structure");
+//         console.log("Go to coding meetup");
+//         break;
+//     case 'tuesday':
+//         console.log("Prepare theory videos");
+//         break
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log("write code examples");
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log("write code examples");
+//         break;
+//     default:
+//         console.log("Not a valid day!");
+//         break;
+// }
+
+
+//ternary operator
+
+const age = 21;
+age>=18 ? console.log("I like to drink wine"): console.log("I like to drink water");
+
+const drink = age>=21 ? 'wine' : 'water'
+console.log(drink);
+
+
+// challenge tip calculator withou if...else 
+
+const bill = 275;
+
+/* Write your code below. Good luck! 🙂 */
+
+const tip = (bill >=50 && bill <=300) ? bill*0.15 : bill * 0.20;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
