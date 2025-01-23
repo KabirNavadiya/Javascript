239a1eec7485588caed27deeca5ea80a38f341a2
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-23 12:59:32 +0530

    datatypes, let,const,var

---
+++ JS-Fundamentals-part-1/index.js
@@ -0,0 +1,77 @@
+// let js = "amazing";
+
+// console.log(40+60-20);
+
+// console.log(js);
+// let firstName = "kabir";
+// console.log(firstName);
+
+
+// // how to define variables . 
+// let jonas_maltida = "JM";
+// let $function =26;
+// let person = "kabir";
+// let PI = 3.1415;
+
+
+// //booolean datatype
+
+// let javaScriptisFun = true;
+// console.log(javaScriptisFun);
+
+// // typeof 
+
+// console.log(typeof true);
+// console.log("type of javascriptisfun before:",typeof javaScriptisFun);
+// console.log(typeof 26);
+
+// //dyynamic typing
+
+// javaScriptisFun = "yes";
+// console.log("type of javascriptisfun after:",typeof javaScriptisFun);
+
+
+//let (mutable)
+let age=30;
+console.log(age);
+
+age = 36;
+console.log(age);
+
+
+console.log("let-block ex :");
+{
+    let job = "programmer";
+    console.log(job);
+    
+
+}
+console.log("outside block",job);
+job = "technician";
+console.log(job);
+
+
+//const (immutable)
+const birthYear = 2003;
+console.log(birthYear);
+
+//can't change. 
+// birthYear = 2002;
+// console.log(birthYear); 
+
+// no empty consts
+// const job; 
+
+// var
+
+
+console.log("car-block ex :");
+
+{
+    var job = "programmer";
+
+}
+console.log(job);
+job = "technician";
+console.log(job);
+
