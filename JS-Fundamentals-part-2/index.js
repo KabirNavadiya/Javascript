164fb3c638e8e0729da0667d3fc297171c1682d3
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-24 18:50:12 +0530

    patterns

--- JS-Fundamentals-part-2/index.js
+++ JS-Fundamentals-part-2/index.js
@@ -485,14 +485,14 @@ const jonasArray = [
 //    **
 //     *
 
-for(let i=1;i<=5;i++){
-    for(let j=1;j<=5;j++){
-        if(j>=i){
-            process.stdout.write("*");
-        }
-        else{
-            process.stdout.write(" ");
-        }
-    }
-    console.log();
-}
+// for(let i=1;i<=5;i++){
+//     for(let j=1;j<=5;j++){
+//         if(j>=i){
+//             process.stdout.write("*");
+//         }
+//         else{
+//             process.stdout.write(" ");
+//         }
+//     }
+//     console.log();
+// }
