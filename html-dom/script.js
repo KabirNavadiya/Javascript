12c74481cd259c2aaed11e86e385b87fe88e1e9f
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-27 16:53:36 +0530

    modifying elements

--- html-dom/script.js
+++ html-dom/script.js
@@ -4,4 +4,16 @@ const body = document.body
 
 const div = document.createElement("div") // creating elements
 
+
+
+//modifying elements
+
+// method1
+// div.innerHTML = "Hello World ! (innerHTML)";
+// div.innerText = "hello World ! (innerText)";
+
+// method2
+// div.textContent= "hello World ! (textcontent)"
+
+
 body.append(div)
