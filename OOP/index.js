eab4eee85664aa980db810a4ae6ca850ffd501f9
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-27 15:38:10 +0530

    challenege 3

--- OOP/index.js
+++ OOP/index.js
@@ -199,3 +199,77 @@
 // console.log(ford);
 
 
+// // Inheritance between "classes" : constructor functions.
+
+// const Person = function(firstName,birthYear){
+//     this.firstName=firstName;
+//     this.birthYear=birthYear;
+
+// };
+// Person.prototype.calcAge = function (){
+//     console.log(2024-this.birthYear);
+    
+// };
+
+// const Student = function(firstName,birthYear,course){
+//     Person.call(this,firstName,birthYear); // inheriting Person property in student. direct function call not works so use .call with it.
+//     this.course = course;
+// };
+
+// // Linking prototypes
+// Student.prototype = Object.create(Person.prototype); // now we can use mike.calcAge();
+
+
+// Student.prototype.introduce = function(){
+//     console.log(`My Name is ${this.firstName} and I study ${this.course}`);
+    
+// }
+// const mike = new Student('Mike',2003,'Computer Science');
+// mike.introduce();
+// mike.calcAge();
+
+
+
+// // # CHALLENEGE 3
+
+
+// const Car = function(make , speed){
+//     this.make=make;
+//     this.speed=speed;
+// };
+
+// Car.prototype.accelerate = function(){
+//     this.speed += 10;
+//     console.log(`${this.make} is going at ${this.speed} km/h`);
+    
+// }
+// Car.prototype.brake = function(){
+//     this.speed -= 5;
+//     console.log(`${this.make} is going at ${this.speed} km/h`);
+// }
+
+// const EV = function(make,speed,charge){
+//     Car.call(this,make,speed);
+//     this.charge = charge;
+// }
+
+// //Linking the prototypes.
+// EV.prototype = Object.create(Car.prototype);
+
+// EV.prototype.chargeBattery=function(chargeTo){
+//     this.charge=chargeTo;
+// }
+
+// EV.prototype.accelerate = function(){
+//     this.speed +=20;
+//     this.charge--;
+//     console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`);
+    
+// }
+// const tesla = new EV('Tesla',120,23);
+// tesla.chargeBattery(90);
+// console.log(tesla);
+// tesla.brake()
+// tesla.accelerate();
+
+
