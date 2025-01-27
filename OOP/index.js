5d049cac4b89afe306d534902d7782804bebe7f0
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-27 14:06:08 +0530

    Static method

--- OOP/index.js
+++ OOP/index.js
@@ -119,22 +119,31 @@
 
 
 
-// GETTERS & SETTERS
+// // GETTERS & SETTERS
 
-const account= {
+// const account= {
 
-    owner:'kabir',
-    movements:[200,530,120,300],
+//     owner:'kabir',
+//     movements:[200,530,120,300],
 
-    get latest(){
-        return this,this.movements.slice(-1).pop();
-    },
-    set latest(mov){
-        this.movements.push(mov);
-    }
-};
+//     get latest(){
+//         return this,this.movements.slice(-1).pop();
+//     },
+//     set latest(mov){
+//         this.movements.push(mov);
+//     }
+// };
 
-console.log(account.latest);
-account.latest = 50;
-console.log(account.movements);
+// console.log(account.latest);
+// account.latest = 50;
+// console.log(account.movements);
 
+
+
+// // STATIC METHOD
+// class StaticMethod {
+//     static hey(){
+//         console.log(`Hey There! Good afternoon.`);     
+//     }
+// }
+// StaticMethod.hey(); 
