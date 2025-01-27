231e6515cc9c16035b6c57996058b6e539dc9a48
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-27 16:47:11 +0530

    creating elements

--- html-dom/script.js
+++ html-dom/script.js
@@ -1,3 +1,7 @@
 const body = document.body
-body.append("Hello World !","Bye")
-// body.appendChild("Hello World !")
+// body.append("Hello World !","Bye")
+// // body.appendChild("Hello World !")
+
+const div = document.createElement("div") // creating elements
+
+body.append(div)
