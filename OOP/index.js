660bbf3f89e9307199bff5aab43670f24011cd92
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-27 14:26:15 +0530

    challenege 2

--- OOP/index.js
+++ OOP/index.js
@@ -147,3 +147,55 @@
 //     }
 // }
 // StaticMethod.hey(); 
+
+
+
+// // Object.create
+
+// const PersonProto = {
+//     calcAge(){
+//         console.log(2024 - this.birthYear);
+//     },
+//     init(firstName,birthYear){
+//         this.firstName=firstName;
+//         this.birthYear=birthYear;
+//     }
+// }
+// const steven = Object.create(PersonProto);
+// steven.init("Steven",2003);
+// steven.calcAge();
+
+
+// // # CHALLENGE 2
+
+// class Carcl {
+//     constructor(make,speed){
+//         this.make=make;
+//         this.speed=speed;
+//     }
+//     accelerate(){
+//         this.speed+=10;
+//         console.log(`${this.make} is going ar ${this.speed} km/h`);
+//     }
+//     brake(){
+//         this.speed-=5;
+//         console.log(`${this.make} is going ar ${this.speed} km/h`);
+//     }
+
+//     get speedUS(){
+//         return this.speed / 1.6;
+//     }
+//     set speedUS(speed){
+//         this.speed = speed * 1.6;
+//     }
+// }
+
+// const ford = new Carcl('Ford',120);
+// console.log(ford.speedUS);
+// ford.accelerate();
+// ford.accelerate();
+// ford.brake();
+// ford.speedUS = 50;
+// console.log(ford);
+
+
