8b71210f978d8022b512e82657a5f1e4be5935b0
KabirNavadiya <kabirnavadia27@gmail.com>
2025-01-29 15:40:50 +0530

    promise chaining error-handling

--- error-handling/promises.js
+++ error-handling/promises.js
@@ -1,27 +1,32 @@
 const cart = ["shoes","pants","kurta"];
 
-
-//flow ->  1,2,3,4,...... see comment
-
-
-const promise = createOrder(cart); //1
-
+//flow ->  1,2,3,4,...... see comments
 
 // called when we resolve a promise
-promise.then(function(orderId){  //6
-    console.log(orderId);
-})
-
-// called when we reject a promise
-.catch((err)=>{ //7
-    console.log(err.message);
-    
-})
+createOrder(cart) //1
+    .then(function(orderId){  //6
+        console.log(orderId);
+        return orderId;
+    })
+    .then(function(orderId){ 
+        // console.log(orderId);
+    //    console.log(proceedToPayment(orderId));
+        return proceedToPayment(orderId); // returns a promise to next call back function.
+        
+    })
+    .then(function(paymentInfo){
+        console.log(paymentInfo);
+        
+    })
+    // called when we reject a promise
+    .catch((err)=>{ //7
+        console.log(err.message);  
+    })
 
 
 function validateCart(cart){  //4 if true then resolved else rejected in this logic
-    // return true;
-    return false;
+    return true;
+    // return false;
 }
 function createOrder(cart){  //2
     const pr = new Promise(function(resolve,reject){
@@ -43,4 +48,13 @@ function createOrder(cart){  //2
         }
     })
     return pr; //5
+}
+
+function proceedToPayment(orderId){
+    // proceed to payment.
+
+    return new Promise((resolve,reject)=>{
+        resolve("Payment success !");
+    })
+
 }
