67ac85ba6f75096313469b33fef699d4fb4786db
KabirNavadiya <kabirnavadia27@gmail.com>
2025-02-07 18:35:22 +0530

    TE-T127 PR-57 Jquery & AJAX : jquery DOM, jquery Effects, get json response with Ajax

--- Jquery/script.js
+++ Jquery/script.js
@@ -1,26 +1,20 @@
-// basic syntax $(selector).action()
-// we write document .ready to prevent loading jquery befor document is loaded.
+
 $(document).ready(function(){
-    // selector can be elements(button) or classname(.hide) or id(#hide).
-    // jquery has method hide() and show()
+
     // $(".hide").click(function(){
     //     $(".a").hide();
     // });
     // $(".show").click(function(){
     //     $(".a").show();
     // });
-
     $(".hide").click(function(){
         $(".a").toggle();
     })
 });
 
 $(document).ready(function(){
-    //paragraph will disappear when double clicked.
     $("#hide-para").dblclick(function(){
-        $(this).hide("fast",
-            //callback function
-            function(){
+        $(this).hide("fast",function(){
             alert("I disappeared");
             window.location.href = "/html-dom/dice-game.html";
         }); 
@@ -37,13 +31,6 @@ $(document).ready(function(){
         $("#mouseenter").hide(500);
     });
 });
-
-// $(document).ready(function(){
-//     $("#mouseenter").hide();
-//     $(".mouseenter").hover(function(){
-//         $("#mouseenter").show();
-//     });
-// });
 
 $(document).ready(function(){
     $(".onexample").on({
@@ -81,21 +68,18 @@ $(document).ready(function(){
 });
 $(document).ready(function(){
     $("#flip").click(function(){
-      $("#panel").slideToggle("slow");
+      $("#panel").slideToggle("fast");
     });
 });
 $(document).ready(function(){
     $("#boxbutton").click(function(){
-        //queue functionality
         var div = $("#p1");
         div.animate({height: '300px', opacity: '0.4'}, "slow");
         div.animate({width: '300px' , opacity: '0.8'}, "slow");
         div.animate({height: '50px', opacity: '0.4'}, "slow");
         div.animate({width: '50px', opacity: '0.8'}, "slow");
-        // can even specify the property's animation as "show", "hide", or "toggle".
     });
 });
-// jquery chaining.
 $(document).ready(function(){
     $("#chain").click(function(){
       $("#p1").slideUp(2000).slideDown(2000);
@@ -109,8 +93,42 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#chglink").click(function(){
       $("#w3s").attr("href", function(i,origValue){
-        // i specifies the index of attribute(href) if multiple found then index will be  0,1,2 ....
         return origValue + "/jquery/"; 
       });
     }); 
 });
+
+$(document).ready(function(){
+    $("#chglink").click(function(){
+        // alert("Text : " + $("#chglink").text());
+       $("#chglink").text("hello World")
+        // alert("Text : " + $("#chglink").html());
+    })
+})
+$(document).ready(function(){
+    $("p").addClass("blue");
+})
+//callback add,fire
+var foo = function(value){
+    console.log("Foo : ",value); 
+}
+var bar = function(value){
+    console.log("Bar : ",value);
+}
+var callbacks = $.Callbacks();
+
+callbacks.add(foo)
+callbacks.add(bar);
+callbacks.fire("Hello");
+
+
+$( window ).on( "resize", function() {
+    $( "#log" ).prepend( "<div>" + $( window ).width() + "</div>" );
+  } );
+$(document).ready(function(){
+   $( ".target" ).on( "change", function() {
+        let opt = document.getElementById("target").value
+        console.log(opt);
+    });
+})
+
